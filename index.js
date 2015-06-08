aacbe68b07f4bc89b2afa0b7578259bcc828a179
seka <sekaryo_shin@cyberagent.co.jp>
2015-06-08 17:31:22 +0900

    [fix] index.jsの内容を修正

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var fs = require('fs');
 var postcss = require('postcss');
-var styleGuide = require('postcss-style-guide');
 
 var css = fs.readFileSync('./src/style.css', 'utf8')
 
 var options = {
-    theme: "pstyle",
-    name: "Project name"
+    theme: 'pstyle',
+    name: 'Project name'
 };
 
 var output = postcss()
-    .use(styleGuide(options))
-    .process(css)
+    .process(css, {
+        from: 'src/simple.css'
+    })
     .css;
 
 fs.writeFileSync('./style.css', output)
